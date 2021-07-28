a3d0c9c3c57fae33deea6e3d77f4ea2c82d73501
lipsa7 <lipsakar98@gmail.com>
2021-07-28 12:06:47 +0530

    Add project files.

---
+++ Rdppractice2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdppractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            Console.WriteLine(UserProgramCode.cowGraze(len));
+
+            Console.ReadKey();
+        }
+    }
+
+    class UserProgramCode
+    {
+        public static string cowGraze(int len)
+        {
+            double area;
+            area = 3.14 * Math.Pow(len, 2);
+
+            return String.Format("{0:0.00}", area.ToString());
+        }
+    }
+}
